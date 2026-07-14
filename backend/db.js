01cbe9aed1e0a400aaab720ed58b75585c0de067
Nakesh_Tewari <nakesh1107@gmail.com>
2026-07-14 11:22:13 +0530

    Add .gitignore file and improve database connection error handling

--- backend/db.js
+++ backend/db.js
@@ -2,10 +2,15 @@ const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+  port: Number(process.env.DB_PORT),
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+
+  ssl: {
+    rejectUnauthorized: false,
+  },
+
   waitForConnections: true,
   connectionLimit: 10,
 });
@@ -15,9 +20,10 @@ const pool = mysql.createPool({
   try {
     const connection = await pool.getConnection();
     console.log("✅ Database connected successfully!");
-    connection.release(); // Return connection to the pool
+    connection.release();
   } catch (err) {
-    console.error("❌ Database connection failed:", err.message);
+    console.error("❌ Database connection failed:");
+    console.error(err);
   }
 })();
 
